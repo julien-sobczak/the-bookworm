2a790c7b4267dbf7727a13175a3c01372fa2efc9
Julien Sobczak <julien.sobczak@gmail.com>
2019-07-08 16:18:37 +0200

    Refactor ColorPicker to use React hooks

--- src/toolbox/ColorPicker.js
+++ src/toolbox/ColorPicker.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { ChromePicker } from 'react-color'; // https://casesandberg.github.io/react-color/#examples
 import styled from 'styled-components';
@@ -6,101 +6,71 @@ import styled from 'styled-components';
 /**
  * Simple color picker based on See https://casesandberg.github.io/react-color/#examples
  */
-class ColorPicker extends React.Component {
+function ColorPicker(props) {
 
-    static propTypes = {
-        // Default selection
-        color: PropTypes.string,
-        onChange: PropTypes.func,
-    };
+    const [displayColorPicker, setDisplayColorPicker] = useState(false);
+    const [color, setColor] = useState(props.color);
 
-    static defaultProps = {
-        color: '#FFF',
-        onChange: undefined,
-    };
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            displayColorPicker: false,
-            color: props.color,
-        };
-
-        this.handleColorChange = this.handleColorChange.bind(this);
-    }
-
-    handleColorClick = () => {
-        this.setState(state => ({
-            ...state,
-            displayColorPicker: !state.displayColorPicker,
-        }))
-    };
-
-    handleColorClose = () => {
-        this.setState(state => ({
-            ...state,
-            displayColorPicker: false,
-        }))
-    };
-
-    handleColorChange = (color) => {
-        const newState = {
-            ...this.state,
-            color: color.hex,
-        }
-        this.setState(newState);
-        if (this.props.onChange) {
-            this.props.onChange(color)
+    const handleColorChange = (c) => {
+        setColor(c.hex)
+        if (props.onChange) {
+            props.onChange(c.hex)
         }
     };
 
-    render() {
+    const Color = styled.div`
+        width: 36px;
+        height: 24px;
+        border-radius: 2px;
+        background: ${color};
+    `;
+    const Swatch = styled.div`
+        padding: 5px;
+        background: '#fff';
+        border-radius: 1px;
+        box-shadow: 0 0 0 1px black;
+        display: inline-block;
+        cursor: pointer;
+    `;
+    const Popover = styled.div`
+        position: absolute;
+        z-index: 2;
+        bottom: 0;
+        left: 0;
+    `;
+    const Cover = styled.div`
+        position: fixed;
+        top: 0px;
+        right: 0px;
+        bottom: 0px;
+        left: 0px;
+    `;
 
-        const Color = styled.div`
-            width: 36px;
-            height: 24px;
-            border-radius: 2px;
-            background: ${this.state.color};
-        `
-        const Swatch = styled.div`
-            padding: 5px;
-            background: '#fff';
-            border-radius: 1px;
-            box-shadow: 0 0 0 1px black;
-            display: inline-block;
-            cursor: pointer;
-        `
-        const Popover = styled.div`
-            position: absolute;
-            z-index: 2;
-            bottom: 0;
-            left: 0;
-        `
-        const Cover = styled.div`
-            position: fixed;
-            top: 0px;
-            right: 0px;
-            bottom: 0px;
-            left: 0px;
-        `
+    return (
 
-        return (
+        <div style={{'position': 'relative'}}>
+            <Swatch onClick={ () => setDisplayColorPicker(!displayColorPicker) }>
+                <Color />
+            </Swatch>
+            { displayColorPicker ?
+                <Popover>
+                    <Cover onClick={ () => setDisplayColorPicker(false) }/>
+                    <ChromePicker color={ color } onChange={ handleColorChange } />
+                </Popover> : null }
+        </div>
 
-            <div style={{'position': 'relative'}}>
-                <Swatch onClick={ this.handleColorClick }>
-                    <Color />
-                </Swatch>
-                { this.state.displayColorPicker ?
-                    <Popover>
-                        <Cover onClick={ this.handleColorClose }/>
-                        <ChromePicker color={ this.state.color } onChange={ this.handleColorChange } />
-                    </Popover> : null }
-            </div>
-
-        );
-    }
-
+    );
 }
+
+ColorPicker.propTypes = {
+    // Default selection
+    color: PropTypes.string,
+    onChange: PropTypes.func,
+};
+
+ColorPicker.defaultProps = {
+    color: '#FFF',
+    onChange: undefined,
+};
 
 export default ColorPicker;
