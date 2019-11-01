ad186ce2267017ebdfb8902c30afbadacda00d77
Julien Sobczak <julien.sobczak@gmail.com>
2019-11-01 11:36:14 +0100

    Implement filters in Books catalog

--- src/js/components/library/LibraryBooks.js
+++ src/js/components/library/LibraryBooks.js
@@ -6,6 +6,8 @@ import Loader from "../toolbox/Loader";
 
 import * as library from "../../functions/library";
 
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+
 const mapStateToProps = state => {
     return { readings: state.readings };
 };
@@ -16,12 +18,28 @@ class LibraryBooks extends React.Component {
         super(props);
 
         this.state = {
+            // Does the catalog is currently being downloading?
             loading:  true,
+            // The catalog retrieved from open-library-books.firebaseapp.com
             catalog: [],
+            // The filtered books
+            books: [],
+            // The custom text filter
+            filterRegex: "",
+            // Which letter is currently selected by the user to filter books?
+            filterLetter: "*",
+            // Which language is currently selected by the user?
+            filterLanguage: "",
         };
 
+        this.filterByRegex = this.filterByRegex.bind(this);
+        this.filterByLanguage = this.filterByLanguage.bind(this);
+        this.filterByLetter = this.filterByLetter.bind(this);
+        this.filterByAuthor = this.filterByAuthor.bind(this);
         this.handleBookSelected = this.handleBookSelected.bind(this);
     }
+
+    /* Selection management */
 
     handleBookSelected(event) {
         const slug = event.target.dataset.slug;
@@ -38,6 +56,71 @@ class LibraryBooks extends React.Component {
         }
     }
 
+    /* Filters management */
+
+    filterBooks() {
+        const letterCount = new Array(letters.length).fill(0);
+        const matchFilters = book => {
+            if (this.state.filterLanguage !== '' && book.language !== this.state.filterLanguage) {
+                return false;
+            }
+
+            if (this.state.filterLetter !== '*' && book.title[0] !== this.state.filterLetter) {
+                return false;
+            }
+
+            if (this.state.filterRegex !== '') {
+                const regex = new RegExp(this.state.filterRegex, 'g');
+                if (book.title.match(regex)) return true;
+                if (book.author.match(regex)) return true;
+                // TODO add Kind
+                return false;
+            }
+
+            const firstLetter = book.title[0].toUpperCase();
+            const indexFirstLetter = letters.indexOf(firstLetter);
+            if (indexFirstLetter !== -1) {
+                letterCount[indexFirstLetter] = letterCount[indexFirstLetter]+1;
+            }            
+            return true;
+        }
+        const books = this.state.catalog.filter(matchFilters);
+        this.setState({
+            books: books,
+            letterCount: letterCount,
+        });
+    }
+
+    filterByRegex(event) {
+        this.setState({
+            filterRegex: event.target.value,
+        }, this.filterBooks);
+    }
+
+    filterByLanguage(event) {
+        let newLanguage = event.target.dataset.language;
+        if (newLanguage === this.state.filterLanguage) {
+            newLanguage = '';
+        }
+        this.setState({
+            filterLanguage: newLanguage,
+        }, this.filterBooks);
+    }
+
+    filterByLetter(event) {
+        this.setState({
+            filterLetter: event.target.dataset.letter,
+        }, this.filterBooks);
+    }
+
+    filterByAuthor(event) {
+        const author = event.target.dataset.author;
+        this.setState({
+            filterLetter: '*',
+            filterRegex: author,
+        }, this.filterBooks);
+    }
+
     render() {
         return (
             <div className="LibraryBooks Centered">
@@ -47,10 +130,51 @@ class LibraryBooks extends React.Component {
                     <>
                         <h3>Choose a book</h3>
                         {/* TODO Add filters by language, by kind, search box */}
+
+                        <div className="LibraryFilters">
+                            
+                            {/* Filter by regex */}
+                            {this.state.filterRegex}
+                            <input type="text" placeholder="Search..." value={this.state.filterRegex} onChange={this.filterByRegex} />
+                            
+                            {/* Filter by language */}
+                            <ul>
+                                {this.state.popularLanguages.map((language, index) => {
+                                    const selected = this.state.filterLanguage === language.name;
+                                    return (
+                                        <li key={index} className={selected ? 'Selected' : ''}>
+                                            <button onClick={this.filterByLanguage} className="Clickable" data-language={language.name}>
+                                                {language.name}&nbsp;
+                                                {!selected && <sup>{language.count}</sup>}
+                                                {selected && <i className="material-icons md-small">backspace</i>}
+                                            </button>
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+
+                            {/* Filter by first letter */}
+                            <ul>
+                                <li key={0} className={this.state.filterLetter === '*' ? 'Selected' : ''}>
+                                    <button onClick={this.filterByLetter} data-letter="*">All</button>
+                                </li>
+                                {letters.map((letter, index) => {
+                                    const countLetter = this.state.letterCount && this.state.letterCount[letters.indexOf(letter)];
+                                    return (
+                                        <li key={index+1} className={this.state.filterLetter === letter ? 'Selected' : ''}>
+                                            {countLetter > 0 && <button onClick={this.filterByLetter} data-letter={letter}>{letter}</button>}
+                                            {countLetter === 0 && <button disabled={true}>{letter}</button>}
+                                        </li>
+                                    );
+                                })}
+                            </ul>
+
+                        </div>
+
                         <div className="Bookshelf Scrollbar">
                             <table>
                                 <tbody>
-                                    {this.state.catalog.map((book, index) => {
+                                    {this.state.books.map((book, index) => {
                                         return (
                                             <tr key={index}>
                                                 <td className="BookTitle">
@@ -59,7 +183,10 @@ class LibraryBooks extends React.Component {
                                                     </button>
                                                 </td>
                                                 <td className="BookAuthor">
-                                                    {book.author}
+                                                    <button onClick={this.filterByAuthor} data-author={book.author}>{book.author}</button>
+                                                </td>
+                                                <td className="BookAuthor">
+                                                    {book.language}
                                                 </td>
                                                 <td>
                                                     {book.slug in this.props.readings &&
@@ -82,13 +209,42 @@ class LibraryBooks extends React.Component {
         );
     }
 
+    static getPopularLanguages(books) {
+        // Keep only the first most present languages
+        const keepLanguages = 5;
+
+        const languageNames = [];
+        const languageCount = [];
+        books.forEach(book => {
+            const index = languageNames.indexOf(book.language);
+            if (index === -1) {
+                languageNames.push(book.language);
+                languageCount.push(0);
+            } else {
+                languageCount[index]++;
+            }
+        });
+
+        // Zip arrays
+        const popularLanguages = languageNames.map(function(name, i) {
+            return {
+                name: name,
+                count: languageCount[i],
+            };
+        });
+
+        return popularLanguages.slice(0, Math.min(popularLanguages.length, keepLanguages));
+    }
+
     componentDidMount() {
         console.log(`Downloading ${this.CATALOG_URL}...`);
         library.downloadCatalog().then(data => {
+            const popularLanguages = LibraryBooks.getPopularLanguages(data);
             this.setState({
                 loading: false,
-                catalog: data
-            });
+                catalog: data,
+                popularLanguages: popularLanguages,
+            }, this.filterBooks);
         });
     }
 }
