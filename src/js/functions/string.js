711c83abc7d01c044404e50b3479e244050833ab
Julien Sobczak <julien.sobczak@gmail.com>
2020-01-02 21:17:48 +0100

    Fix bug in capitalize

--- src/js/functions/string.js
+++ src/js/functions/string.js
@@ -6,7 +6,7 @@
  * @returns {string} The same text capitalized
  */
 export function capitalize(s) {
-    if (typeof s !== 'string') return '';
+    if (typeof s !== 'string') return '' + s;
     return s.charAt(0).toUpperCase() + s.slice(1);
 }
 
