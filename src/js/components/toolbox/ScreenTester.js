5e327f2f4d9b8fe991663244f7a485e23f7ea66e
Julien Sobczak <julien.sobczak@gmail.com>
2020-12-27 23:11:27 +0100

    Add screen for small devices

---
+++ src/js/components/toolbox/ScreenTester.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+function ScreenTester({minWidth, minHeight}) {
+
+    const Container = styled.div`
+        /* Hidden by default */
+        visibility: hidden;
+
+        /* Cover the scren */
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+
+        /* Style the text */
+        font-family: "Roboto Slab", cursive;
+        background: linear-gradient(0deg,
+            var(--home-color-8) 0%,    var(--home-color-8) 12.5%,
+            var(--home-color-7) 12.5%, var(--home-color-7) 25%,
+            var(--home-color-6) 25%,   var(--home-color-6) 37.5%,
+            var(--home-color-5) 37.5%, var(--home-color-5) 50%,
+            var(--home-color-4) 50%,   var(--home-color-4) 62.5%,
+            var(--home-color-3) 62.5%, var(--home-color-3) 75%,
+            var(--home-color-2) 75%,   var(--home-color-2) 87.5%,
+            var(--home-color-1) 87.5%, var(--home-color-1) 100%
+        );
+        letter-spacing: 1px;
+
+        /* Center the text */
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        svg {
+            width: 90%;
+        }
+        svg text {
+            fill: black;
+            paint-order: stroke;
+            stroke: white;
+            stroke-width: 4px;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+            font-weight: 800;
+        }
+
+        @media (max-width: ${minWidth}), (max-height: ${minHeight}) {
+            visibility: visible;
+            z-index: 999;
+        }
+    `;
+
+    return (
+        <Container>
+            <svg viewBox="0 0 62 50">
+                <text x="3" y="15">Screen</text>
+                <text x="3" y="30">Too</text>
+                <text x="3" y="45">Small</text>
+            </svg>
+        </Container>
+    );
+}
+
+ScreenTester.propTypes = {
+    minWidth: PropTypes.string.isRequired,
+    minHeight: PropTypes.string.isRequired,
+};
+
+export default ScreenTester;
