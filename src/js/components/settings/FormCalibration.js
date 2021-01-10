d4d2af0663979d8d650d4913b19b5e07c42bee6d
Julien Sobczak <julien.sobczak@gmail.com>
2021-01-10 10:40:23 +0100

    Implement first draft of Tutorial

--- src/js/components/settings/FormCalibration.js
+++ src/js/components/settings/FormCalibration.js
@@ -1,36 +1,14 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
-import Help from '../toolbox/Help';
-import Slider from '../toolbox/Slider';
-
-const StepVisualizer = () => {
-    const stylesBar = {
-        marginLeft: "0.5em",
-        backgroundColor: "black",
-        color: "white",
-        textAlign: "center",
-        padding: "0.5em 0"
-    };
-    const stylesLegend = {
-        margin: "0.5em",
-        fontWeight: "normal",
-    };
-    // We increment by "0.25in"
-    return (
-        <>
-            <span className="Width0_25in" style={stylesBar}></span> <small style={stylesLegend}>👈 Must measure ideally 0.25in on your screen</small>
-        </>
-    );
-};
+import CalibrationPreview from './CalibrationPreview';
 
 const FormCalibration = (props) => {
 
     const [displayScale, setDisplayScale] = useState(props.displayScale);
     const onChange = props.onChange;
 
-    const handleDisplayScaleChange = (event) => {
-        const newValue = parseFloat(event.target.value);
+    const handleDisplayScaleChange = (newValue) => {
         if (displayScale === newValue) return;
         setDisplayScale(newValue);
         onChange({
@@ -39,28 +17,17 @@ const FormCalibration = (props) => {
     };
 
     return (
-        <table className="Setting">
-            <tbody>
-                <tr>
-                    <th>
-                        <label htmlFor="displayScale">Length calibration</label>:
-                    </th>
-                    <td>
-                        <Help title="Determine the increment in space for every drill level" />
-                    </td>
-                    <td>
-                        <Slider id="displayScale" min={0.5} value={displayScale} max={2} onChange={handleDisplayScaleChange} />
-                        <StepVisualizer/>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <CalibrationPreview value={displayScale} onChange={handleDisplayScaleChange} />
     );
 };
 
 FormCalibration.propTypes = {
     displayScale: PropTypes.number,
     onChange: PropTypes.func,
+};
+FormCalibration.defaultProps = {
+    displayScale: 1,
+    onChange: () => {},
 };
 
 export default FormCalibration;
